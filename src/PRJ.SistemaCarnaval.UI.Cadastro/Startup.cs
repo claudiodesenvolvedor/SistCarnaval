84af0bea5b9ed5f2e3cfb3e4e1df43aae7c70c13
claudiodesenvolvedor <claudio.desenvolvedor@gmail.com>
2017-11-29 20:50:07 -0200

    Inicio Projeto Sistema do Carnaval

---
+++ src/PRJ.SistemaCarnaval.UI.Cadastro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PRJ.SistemaCarnaval.UI.Cadastro.Startup))]
+namespace PRJ.SistemaCarnaval.UI.Cadastro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
