0690a6c3f319fb0baf89157c1ccf6be08c0bcf3f
claudiodesenvolvedor <claudio.desenvolvedor@gmail.com>
2017-12-04 01:13:16 -0200

    Criação da tabela ContaBancaria

---
+++ src/PRJ.SistemaCarnaval.Domain/Entities/ContaBancaria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PRJ.SistemaCarnaval.Domain.Entities
+{
+    public class ContaBancaria
+    {
+        public Guid ContaBancariaId { get; set; }   
+     
+        public int TipoConta { get; set; }
+        public string Agencia { get; set; }
+        public string Operacao { get; set; }
+        public string Conta { get; set; }
+        public string Digito { get; set; }
+
+        public virtual Candidato CandidatoLista { get; set; }
+    }
+}
