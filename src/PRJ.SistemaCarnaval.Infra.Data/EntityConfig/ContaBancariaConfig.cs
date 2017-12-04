b9926ac1a9793a0d4908de5764cdc9c60345eee4
claudiodesenvolvedor <claudio.desenvolvedor@gmail.com>
2017-12-04 01:15:35 -0200

    Criação da classe ContaBancariaConfig

---
+++ src/PRJ.SistemaCarnaval.Infra.Data/EntityConfig/ContaBancariaConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using PRJ.SistemaCarnaval.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace PRJ.SistemaCarnaval.Infra.Data.EntityConfig
+{
+    public class ContaBancariaConfig : EntityTypeConfiguration<ContaBancaria>
+    {
+        public ContaBancariaConfig()
+        {
+            HasKey(cb => cb.ContaBancariaId);
+
+            Property(cb => cb.TipoConta)
+                .IsRequired();
+
+            Property(cb => cb.Agencia)
+                .HasMaxLength(5)
+                .IsRequired();
+
+            Property(cb => cb.Conta)
+                .HasMaxLength(8)
+                .IsRequired();
+
+            Property(cb => cb.Digito)
+                .HasMaxLength(1)
+                .IsRequired();
+
+            Property(cb => cb.Operacao)
+                .HasMaxLength(3)
+                .IsOptional();
+
+            ToTable("ContasBancarias");
+        }
+    }
+}
